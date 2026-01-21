e740576570b3636fdc307c4ab151afb93a751595
Stéphane Leblanc <stephane.leblanc@montreal.ca>
2026-01-21 14:49:41 +0000

    feat: cleanup getting started

--- template/test/allTests.apiTestSuite.ts
+++ template/test/allTests.apiTestSuite.ts
@@ -1,5 +1,3 @@
-import { helloApiTests } from "./gettingStarted/hello.apiTest";
 import { waterServiceConnectionApiTests } from "./waterServiceConnections/waterServiceConnection.apiTest";
 
-helloApiTests();
 waterServiceConnectionApiTests();
